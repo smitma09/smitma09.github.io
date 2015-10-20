965336dffc5677f48694285c65fb1557f39c10d8
smitma09 <smitma09@luther.edu>
2015-10-20 11:33:59 -0500

    Update finalTodo.js

--- finalTodo.js
+++ finalTodo.js
@@ -1,1 +1,20 @@
-Copy/paste script from finalTodo.html in here eventually...
+addTask = function() {
+  mainList = document.querySelector("#todoList");
+  task = document.createElement("li");
+  inputValue = document.querySelector("#task").value;
+  taskText = document.createTextNode(inputValue);
+  priorityValue = document.querySelector("#priority").value;
+    
+  checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.onclick = check;
+ 
+  mainList.appendChild(task);
+  task.appendChild(checkbox);
+  task.appendChild(taskText);
+  task.className = priorityValue; <!-- Lets CSS rules color task based on priority-->
+}
+        
+check = function() {
+  this.parentNode.classList.add("finished");
+}
